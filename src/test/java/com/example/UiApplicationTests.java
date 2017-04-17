d6ed40ce61c6679ca50c783dbd6ad657774f2968
Jan Sindberg <jan.sindberg@gmail.com>
2017-04-17 11:22:03 +0200

    Figuring out setup with hibernate/JPA

--- src/test/java/com/example/UiApplicationTests.java
+++ src/test/java/com/example/UiApplicationTests.java
@@ -2,15 +2,26 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UiApplicationTests {
 
+	@Autowired
+    UserDao userDao;
+
 	@Test
 	public void contextLoads() {
+		User user = new User("UserName", "password");
+		userDao.save(user);
+		User result = userDao.findOne(user.getId());
+		assertTrue(result.equals(user));
+
 	}
 
 }
